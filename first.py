9740a96784cc584050787d8bd8a1c0d346d54b39
Basavaraj4321 <bms4495@gmail.com>
2018-08-25 15:27:05 +0530

    Added_dict

--- first.py
+++ first.py
@@ -1,3 +1,10 @@
 print('hello world')
 #added a new line 
 print("hello world")
+
+#new changes to test git diff
+new_l = [1,2,3,4]
+new_l.index(3)
+
+dict1 = {1:'Basu', 2:'Kiran', 3:'Akash'}
+print(dict1)
