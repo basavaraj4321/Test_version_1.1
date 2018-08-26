5386ed673b248a0882cb4b08551b1fe6b0d752cc
Basavaraj4321 <bms4495@gmail.com>
2018-08-26 08:51:34 +0530

    Added logic to print numbers from 1 to 100

--- first.py
+++ first.py
@@ -7,12 +7,12 @@ new_l = [1,2,3,4]
 new_l.index(3)
 
 dict1 = {1:'Basu', 2:'Kiran', 3:'Akash'}
-<<<<<<< HEAD
-print(dict1)
-=======
 print(dict1)
 
 #added set
 set1 = {1,2,3,2}
 print(set1)
->>>>>>>
+
+#Added logic to print number from 1 to 100
+for i in range(1,100):
+    print(i)
